8065e8fcb32a1f417b97ae8c22edfc41e0f5392b
W3129 <W3129@lab.aca>
2018-01-17 08:29:15 -0500

    arreglar static

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'proyecto.urls'
@@ -123,7 +124,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# LOGIN SETTINGS
+LOGIN_REDIRECT_URL = '/'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+
+
 PROJECT_PATH = os.path.split(os.path.abspath(os.path.dirname('proyecto/')))[0]
 TEMPLATE_DIRS = (
     os.path.join(PROJECT_PATH,'login/templates/'),
